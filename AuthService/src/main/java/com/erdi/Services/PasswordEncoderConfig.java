f07255e89dafdf5781a3202f90884a419b05835d
ErdiSyla <erdisyla6@gmail.com>
2025-02-15 17:59:42 +0100

    Commit 35: Changed name of the PasswordEncoder class to PasswordEncoderConfig after it collided with another bean.

--- AuthService/src/main/java/com/erdi/Services/PasswordEncoderConfig.java
+++ AuthService/src/main/java/com/erdi/Services/PasswordEncoderConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-class PasswordEncoder {
+class PasswordEncoderConfig {
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
