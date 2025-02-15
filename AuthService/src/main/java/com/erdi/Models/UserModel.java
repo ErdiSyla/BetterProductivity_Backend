81d42a5ab0faeec6bae223b47f1ffd754d7add31
ErdiSyla <erdisyla6@gmail.com>
2025-02-15 11:04:18 +0100

    Commit 29: 1.Added getters to the UserModel class aswell as a few annotations to help with better Hibernate mapping (Table and Column). 2.Added basic CRUD test operations for UserRepository test (All but for updating credentials, this will be added in the Service Layer).

--- AuthService/src/main/java/com/erdi/Models/UserModel.java
+++ AuthService/src/main/java/com/erdi/Models/UserModel.java
@@ -2,8 +2,11 @@ package com.erdi.Models;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
+@Table(name = "user_model")
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,9 +17,13 @@ public class UserModel {
 	@SequenceGenerator(name = "userModelSeq", sequenceName = "user_model_sequence")
 	private Integer id;
 
+	@Column(name = "username",nullable = false, unique = false)
 	private String username;
 
+	@Column(name = "email", nullable = false, unique = true)
 	private String email;
 
+	@Column(name = "password", nullable = false)
 	private String password;
+
 }
--- AuthService/src/test/java/com/erdi/Repositories/UserRepositoryTest.java
+++ AuthService/src/test/java/com/erdi/Repositories/UserRepositoryTest.java
@@ -2,26 +2,71 @@ package com.erdi.Repositories;
 
 import com.erdi.Models.UserModel;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.annotation.Testable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 @Testable
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class UserRepositoryTest {
 
-	private UserRepository userRepository;
+	private final UserRepository userRepository;
 
 	private UserModel testUserModel1;
-	private UserModel testUserModel2;
-	private UserModel testUserModel3;
 
+	public UserRepositoryTest(@Autowired UserRepository userRepository){
+		this.userRepository = userRepository;
+	}
 	@BeforeEach
 	public void setUp(){
 		testUserModel1 = new UserModel(null,"Erdi Syla","repotest@gmail.com","test pass");
-		testUserModel1 = new UserModel(null,"Erdi Syla 1","repotest1@gmail.com","test pass 1");
-		testUserModel1 = new UserModel(null,"Erdi Syla 2","repotest2@gmail.com","test pass 2");
+
+	}
+
+	@Test
+	public void UserRepository_Save_ReturnsUserTest(){
+		UserModel user = userRepository.saveAndFlush(testUserModel1);
+
+		assertThat(user).isNotNull();
+		assertThat(user.getId()).isEqualTo(testUserModel1.getId());
+		assertThat(user.getUsername()).isEqualTo(testUserModel1.getUsername());
+		assertThat(user.getEmail()).isEqualTo(testUserModel1.getEmail());
+		assertThat(user.getPassword()).isEqualTo(testUserModel1.getPassword());
+	}
+
+	@Test
+	public void UserRepository_FindById_ReturnsUserTest(){
+		int id = userRepository.saveAndFlush(testUserModel1).getId();
+		UserModel returnedUser = null;
+		if(userRepository.findById(id).isPresent()) {
+			returnedUser = userRepository.findById(id).get();
+		} else {
+			fail("User was not saved properly");
+		}
+
+		assertThat(returnedUser).isNotNull();
+		assertThat(returnedUser.getId()).isEqualTo(testUserModel1.getId());
+		assertThat(returnedUser.getUsername()).isEqualTo(testUserModel1.getUsername());
+		assertThat(returnedUser.getEmail()).isEqualTo(testUserModel1.getEmail());
+		assertThat(returnedUser.getPassword()).isEqualTo(testUserModel1.getPassword());
+	}
+
+	@Test
+	public void UserRepository_Delete_ReturnsNothingTest(){
+		int id = userRepository.saveAndFlush(testUserModel1).getId();
+		userRepository.deleteById(id);
+		Optional<UserModel> deletedUser = userRepository.findById(id);
+
+		assertThat(deletedUser).isNotNull();
+		assertThat(deletedUser).isEmpty();
 	}
 }
