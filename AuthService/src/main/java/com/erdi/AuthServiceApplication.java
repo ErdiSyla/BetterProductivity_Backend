e4b69eb73fa4685d5f9b6a832c176f4fa19a3163
ErdiSyla <erdisyla6@gmail.com>
2025-02-19 17:07:37 +0100

    Commit 52: Got rid of the EnableScheduling annotaion since schdeuled tasks will be completed with Kubernetes CronJobs

--- AuthService/src/main/java/com/erdi/AuthServiceApplication.java
+++ AuthService/src/main/java/com/erdi/AuthServiceApplication.java
@@ -2,9 +2,7 @@ package com.erdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class AuthServiceApplication {
 	public static void main(String[] args) {
