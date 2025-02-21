fd7b6890fe53142db948b027d027ea5572db59fa
Erdi Syla <erdisyla6@gmail.com>
2025-02-21 16:01:29 +0100

    Commit 54: Got rid of time of creation for the AuthServoce TokenKeyDTO since it does not need to know it.

--- AuthService/src/main/java/com/erdi/DTO/TokenKeyDTO.java
+++ AuthService/src/main/java/com/erdi/DTO/TokenKeyDTO.java
@@ -2,8 +2,6 @@ package com.erdi.DTO;
 
 import com.erdi.Models.KeyActivity;
 
-import java.time.Instant;
-
 public record TokenKeyDTO (Integer id, String publicKey, String privateKey,
-						   KeyActivity keyActivity, Instant timeOfCreation){
+                           KeyActivity keyActivity){
 }
