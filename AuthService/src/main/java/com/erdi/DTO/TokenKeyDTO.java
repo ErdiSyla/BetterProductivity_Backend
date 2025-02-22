e7e6e6786354979e80719d04452495a029d85214
Erdi Syla <erdisyla6@gmail.com>
2025-02-22 20:57:54 +0100

    Commit 58: Removed KeyActivity field from TokenKeyDTO record.

--- AuthService/src/main/java/com/erdi/DTO/TokenKeyDTO.java
+++ AuthService/src/main/java/com/erdi/DTO/TokenKeyDTO.java
@@ -1,7 +1,4 @@
 package com.erdi.DTO;
 
-import com.erdi.Models.KeyActivity;
-
-public record TokenKeyDTO (Integer id, String publicKey, String privateKey,
-                           KeyActivity keyActivity){
+public record TokenKeyDTO (Integer id, String publicKey, String privateKey){
 }
