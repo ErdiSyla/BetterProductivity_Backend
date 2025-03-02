aec59fd73b31fb64e86ab88e1b16cc43b3645c94
Erdi Syla <erdisyla6@gmail.com>
2025-03-02 13:38:56 +0100

    Commit 74: Changed KafkaProducerService send method to have logging and not cause any big errors.

--- KeyService/src/main/java/com/erdi/Services/KafkaProducerService.java
+++ KeyService/src/main/java/com/erdi/Services/KafkaProducerService.java
@@ -1,16 +1,24 @@
 package com.erdi.Services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaProducerService {
 
     private final KafkaTemplate<String , String> kafkaTemplate;
 
     public void sendMessage(String topic, String message) {
-        kafkaTemplate.send(topic,message);
+        try{
+            kafkaTemplate.send(topic,message);
+            log.info("Message sent to topic {} successfully.", topic);
+        }catch (Exception e){
+            log.error("Error sending message to Kafka topic {}: {}", topic, e.getMessage());
+        }
+
     }
 }
