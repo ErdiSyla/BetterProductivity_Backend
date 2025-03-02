e6b8f4433812df96c4df7480fdb636628f452c82
Erdi Syla <erdisyla6@gmail.com>
2025-03-02 13:30:36 +0100

    Commit 72: Removed InvalidAlgorithmException as it will not be needed.

--- KeyService/src/main/java/com/erdi/Exceptions/InvalidAlgorithmException.java
+++
@@ -1,7 +0,0 @@
-package com.erdi.Exceptions;
-
-public class InvalidAlgorithmException extends RuntimeException {
-    public InvalidAlgorithmException(String message) {
-        super(message);
-    }
-}
