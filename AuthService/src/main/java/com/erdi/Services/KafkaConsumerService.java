c9ab3097bafd991e5c459322535abf5cce5a557a
ErdiSyla <erdisyla6@gmail.com>
2025-03-11 17:20:47 +0100

    Commit 90: Created new class KafkaConsumerService that will be used to consume messages from the Kafka brokers on topics auth-service-keys and key-change.

---
+++ AuthService/src/main/java/com/erdi/Services/KafkaConsumerService.java
@@ -0,0 +1,47 @@
+package com.erdi.Services;
+
+import com.erdi.DTO.TokenKeyDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Getter
+@Service
+@Slf4j
+public class KafkaConsumerService {
+
+	private volatile List<TokenKeyDTO> cachedAuthKeys;
+
+	@KafkaListener(topics = {"auth-service-keys"}, groupId = "auth-group")
+	public void listenAuthKeys(String message){
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			log.info("Received new auth keys : {}", message);
+			cachedAuthKeys = objectMapper.readValue(
+					message, new TypeReference<>() {
+					});
+			log.info("Successfully parsed and cached token keys {}", cachedAuthKeys);
+		} catch (JsonProcessingException e){
+			log.error("Failed to parse token keys from message: {}\nError: {}",message,e.getMessage());
+		} catch (Exception e){
+			log.error("Unexpected error while processing auth-service-keys message: {}\nError: {}", message, e.getMessage());
+		}
+	}
+
+	@KafkaListener(topics = "key-change", groupId = "auth-group")
+	public void listenKeyChanged(String message){
+		try{
+			log.info(message);
+		}catch (Exception e){
+			log.error("Error processing key change message: {}\nError:{}",message,e.getMessage());
+		}
+	}
+
+}
