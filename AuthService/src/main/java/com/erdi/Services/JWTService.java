1a31f3cc8bcf6118d70cac2c59b16b6de760b41c
Erdi Syla <erdisyla6@gmail.com>
2025-03-15 17:07:54 +0100

    Commit 97: 1.Added new method to JWTService class - getRandomActiveKey, a method used to randomly pick a key that will be encrypt the JWT token. 2.Created new JWTServiceTest test class that will be used to test JWTService's methods (no tests in it right now since there is no need to test a private method.

--- AuthService/src/main/java/com/erdi/Services/JWTService.java
+++ AuthService/src/main/java/com/erdi/Services/JWTService.java
@@ -1,9 +1,17 @@
 package com.erdi.Services;
 
+import com.erdi.Models.ErrorCode;
+import com.erdi.DTO.TokenKeyDTO;
+import com.erdi.Exceptions.Implementation.NoActiveKeysAvailableException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +21,24 @@ public class JWTService {
     private final KafkaConsumerService kafkaConsumerService;
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private TokenKeyDTO getRandomActiveKey(){
+        List<TokenKeyDTO> activeKeys = kafkaConsumerService.getCachedAuthKeys();
+        return Optional.ofNullable(activeKeys)
+                .filter(keys -> !keys.isEmpty())
+                .map(keys ->{
+                        if(keys.size() == 1) {
+                            return keys.getFirst();
+                    }
+                        return keys
+                                .get(ThreadLocalRandom.current().nextInt(keys.size()));
+                })
+                .orElseThrow(() -> {
+                    log.error("No active JWT keys available at {}. errorCode: {}",
+                            Instant.now(), ErrorCode.JWT_NO_KEYS.getCode());
+                    return new NoActiveKeysAvailableException
+                            ("No active keys available for JWT generation", ErrorCode.JWT_NO_KEYS);
+                });
+    }
 
 }
---
+++ AuthService/src/test/java/com/erdi/Services/JWTServiceTest.java
@@ -0,0 +1,10 @@
+package com.erdi.Services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.annotation.Testable;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@Testable
+@ExtendWith(MockitoExtension.class)
+public class JWTServiceTest {
+}
