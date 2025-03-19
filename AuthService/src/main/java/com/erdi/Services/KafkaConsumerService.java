259d2707cabe6720b5c996dd7783e9e93b9758f3
ErdiSyla <erdisyla6@gmail.com>
2025-03-19 12:20:52 +0100

    Commit 100: Got rid of unnecessary import in KafkaConsumerService.

--- AuthService/src/main/java/com/erdi/Services/KafkaConsumerService.java
+++ AuthService/src/main/java/com/erdi/Services/KafkaConsumerService.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
