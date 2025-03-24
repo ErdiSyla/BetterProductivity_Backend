51cf97b22f6fcbfae3ccb68d5551da3397f48a36
ErdiSyla <erdisyla6@gmail.com>
2025-03-24 16:30:32 +0100

    Commit 105: Made the decision that AuthService will not handle CSRF token issuing but it will instead be handled by the Api Gateway.

--- AuthService/src/main/java/com/erdi/Config/SecurityConfig.java
+++ AuthService/src/main/java/com/erdi/Config/SecurityConfig.java
@@ -7,7 +7,6 @@
 	import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 	import org.springframework.security.config.http.SessionCreationPolicy;
 	import org.springframework.security.web.SecurityFilterChain;
-	import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 	@Configuration
 	public class SecurityConfig {
@@ -19,22 +18,14 @@
 
 		@Bean
 		public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-			CookieCsrfTokenRepository csrfTokenRepository = customCookieCsrfTokenRepository();
 			http
-					.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-					.csrf(csrf -> csrf
-							.csrfTokenRepository(csrfTokenRepository)
-							.ignoringRequestMatchers("/**"))
+					.sessionManagement(session ->
+							session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+					.csrf(csrf -> csrf.disable())
 					.authorizeHttpRequests(auth -> auth
 							.requestMatchers("/auth/**").permitAll()
 							.anyRequest().authenticated());
 
 			return http.build();
-		}
-
-		private CookieCsrfTokenRepository customCookieCsrfTokenRepository() {
-			CookieCsrfTokenRepository repository = CookieCsrfTokenRepository.withHttpOnlyFalse();
-			repository.setCookieCustomizer(cookieBuilder -> cookieBuilder.secure(false));
-			return repository;
 		}
 	}
