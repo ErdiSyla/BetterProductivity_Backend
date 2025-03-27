5bb4223640b4d4af362f4b2eefb6e17fe76e6085
Erdi Syla <erdisyla6@gmail.com>
2025-03-27 17:19:09 +0100

    Commit 116: Added base test class to JWTService.

--- AuthService/src/test/java/com/erdi/Services/JWTServiceTest.java
+++ AuthService/src/test/java/com/erdi/Services/JWTServiceTest.java
@@ -1,10 +1,36 @@
 package com.erdi.Services;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.commons.annotation.Testable;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import static org.mockito.Mockito.mock;
 
 @Testable
 @ExtendWith(MockitoExtension.class)
 public class JWTServiceTest {
+
+    @Autowired
+    private KafkaConsumerService mockKafkaConsumerService;
+
+    @InjectMocks
+    private JWTService jwtService;
+
+    @Test
+    public void JWTService_generateToken_ReturnsResponseTest(){
+
+    }
+
+    @TestConfiguration
+    static class MockConfig {
+        @Bean
+        public KafkaConsumerService mockKafkaConsumerService() {
+            return mock(KafkaConsumerService.class);
+        }
+    }
 }
