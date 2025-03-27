a99044d91fa134586276cbfe2def180ea5c06cad
Erdi Syla <erdisyla6@gmail.com>
2025-03-27 17:33:26 +0100

    Commit 117: Added new setUp method to setUp needed test components.

--- AuthService/src/test/java/com/erdi/Services/JWTServiceTest.java
+++ AuthService/src/test/java/com/erdi/Services/JWTServiceTest.java
@@ -1,5 +1,6 @@
 package com.erdi.Services;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.commons.annotation.Testable;
@@ -8,6 +9,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.util.Base64;
 
 import static org.mockito.Mockito.mock;
 
@@ -20,6 +25,25 @@ public class JWTServiceTest {
 
     @InjectMocks
     private JWTService jwtService;
+
+    private KeyPair keyPair1;
+    private KeyPair keyPair2;
+    private String keyPair1PrivateKeyBase64;
+    private String keyPair2PrivateKeyBase64;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        keyPair1 = keyGen.generateKeyPair();
+        keyPair2 = keyGen.generateKeyPair();
+
+        keyPair1PrivateKeyBase64 = Base64.getEncoder()
+                .encodeToString(keyPair1.getPrivate().getEncoded());
+        keyPair2PrivateKeyBase64 = Base64.getEncoder()
+                .encodeToString(keyPair2.getPrivate().getEncoded())''
+    }
+
 
     @Test
     public void JWTService_generateToken_ReturnsResponseTest(){
