7ceff50e2d4b5d2dc89f7bd9915e849ab63139a9
Erdi Syla <erdisyla6@gmail.com>
2025-04-07 14:04:47 +0200

    Commit 129: Renamed both application classes and application test classes to their correct names.

--- Key/src/main/java/com/erdi/KeyApplication.java
+++ Key/src/main/java/com/erdi/KeyApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KeyServiceApplication {
+public class KeyApplication {
 	public static void main (String[] args){
-		SpringApplication.run(KeyServiceApplication.class);
+		SpringApplication.run(KeyApplication.class);
 	}
 }
--- Key/src/test/java/com/erdi/KeyApplicationTest.java
+++ Key/src/test/java/com/erdi/KeyApplicationTest.java
@@ -3,8 +3,8 @@ package com.erdi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = KeyServiceApplication.class)
-class KeyServiceApplicationTest {
+@SpringBootTest(classes = KeyApplication.class)
+class KeyApplicationTest {
 
 	@Test
 	void load(){
