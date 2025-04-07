d9bdfc41acc71198bfe96d6ec827f20aa1719b0d
Erdi Syla <erdisyla6@gmail.com>
2025-04-07 14:11:53 +0200

    Commit 132: 1.Changed the code layout in KeyApplication to make it more readable. 2.Added version to the maven-compiler-plugin.

--- Key/src/main/java/com/erdi/KeyApplication.java
+++ Key/src/main/java/com/erdi/KeyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KeyApplication {
-	public static void main (String[] args){
-		SpringApplication.run(KeyApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(KeyApplication.class, args);
 	}
 }
