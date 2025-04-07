106ca0119e3e09cdb65f036ce8e93fdfd0fccd09
Erdi Syla <erdisyla6@gmail.com>
2025-04-07 14:52:52 +0200

    Commit 133: Changed the names of AuthServiceApplication and AuthServiceApplicationTest to AuthenticatioApplication and AuthenticationApplicationTest.

--- Authentication/src/test/java/com/erdi/AuthenticationApplicationTest.java
+++ Authentication/src/test/java/com/erdi/AuthenticationApplicationTest.java
@@ -2,8 +2,10 @@ package com.erdi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(classes = AuthServiceApplication.class)
+@SpringBootTest(classes = AuthenticationApplication.class)
+@TestPropertySource(properties = "KAFKA_CONSUMER_GROUP_ID = test-group")
 class AuthenticationApplicationTest {
 	@Test
 	void load(){
