72ce6224e8449093bdb3b5f517d221be9eef67ef
Erdi Syla <erdisyla6@gmail.com>
2025-04-17 19:48:11 +0200

    Commit 153: Fixed KeyApplicationTest and made only load an empty test config so it doesn't throw errors.

--- Key/src/test/java/com/erdi/KeyApplicationTest.java
+++ Key/src/test/java/com/erdi/KeyApplicationTest.java
@@ -2,10 +2,15 @@ package com.erdi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootTest(classes = KeyApplication.class)
+@SpringBootTest(classes = KeyApplicationTest.TestConfig.class)
 class KeyApplicationTest {
 
+	@Configuration
+	static class TestConfig{
+
+	}
 	@Test
 	void load(){
 
