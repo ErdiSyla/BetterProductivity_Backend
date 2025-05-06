5fcb7fafbfce432eb1aaa002a3ac29bf2a1ab943
Erdi Syla <erdisyla6@gmail.com>
2025-05-06 18:52:48 +0200

    Commit 183: Added KafkaConsumerService to ApiGateway.

---
+++ ApiGateway/src/main/java/com/erdi/Services/KafkaConsumerService.java
@@ -0,0 +1,51 @@
+package com.erdi.Services;
+
+import com.erdi.DTOs.TokenKeyDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+@Service
+@Slf4j
+public class KafkaConsumerService {
+
+    private volatile List<TokenKeyDTO> jwtKeys = Collections.emptyList();
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @KafkaListener(topics = {"validation-keys"}, groupId = "${KAFKA_CONSUMER_GROUP_ID}")
+    public void listenAuthKeys(String message){
+        try {
+            log.info("Received new customer keys : {}", message);
+            jwtKeys = Collections.unmodifiableList(objectMapper.readValue(
+                    message, new TypeReference<List<TokenKeyDTO>>() {
+                    }));
+            log.info("Successfully parsed validation keys {}", jwtKeys);
+        } catch (JsonProcessingException e){
+            log.error("Failed to parse validation keys from message: {}\nError: {}",message,e.getMessage());
+        } catch (Exception e){
+            log.error("Unexpected error while processing message from validation-keys topic: {}\nError: {}",
+                    message, e.getMessage());
+        }
+    }
+
+    @KafkaListener(topics = "key-change", groupId = "${KAFKA_CONSUMER_GROUP_ID}")
+    public void listenKeyChanged(String message){
+        try{
+            log.info(message);
+        }catch (Exception e){
+            log.error("Error processing message from key-change topic: {}\nError:{}",message,e.getMessage());
+        }
+    }
+
+}
