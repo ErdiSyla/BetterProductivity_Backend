e9edde57b2d2537af5c21c58b779b6b56f543e0c
Erdi Syla <erdisyla6@gmail.com>
2025-05-10 18:29:21 +0200

    Commit 186: Changed KafkaConsumerService so that it now uses a map instead of a list in order to better find keys by their key id.

--- Customer/src/main/java/com/erdi/Services/ExtraBeanConfig.java
+++ Customer/src/main/java/com/erdi/Services/ExtraBeanConfig.java
@@ -1,15 +1,20 @@
 package com.erdi.Services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-class PasswordEncoderConfig {
+class ExtraBeanConfig {
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder(12);
+	}
+	@Bean
+	public ObjectMapper objectMapper(){
+		return new ObjectMapper();
 	}
 }
 
--- Customer/src/main/java/com/erdi/Services/KafkaConsumerService.java
+++ Customer/src/main/java/com/erdi/Services/KafkaConsumerService.java
@@ -1,22 +1,42 @@
 package com.erdi.Services;
 
-import com.erdi.DTOs.TokenKeyDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
-import java.util.List;
+import java.util.Map;
 
 @Getter
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class KafkaConsumerService {
+
+	private volatile Map<Integer,String> jwtKeys = Collections.emptyMap();
+
+	private final ObjectMapper objectMapper;
+
+	@KafkaListener(topics = {"customer-keys"}, groupId = "${KAFKA_CONSUMER_GROUP_ID}")
+	public void listenAuthKeys(String message){
+		try {
+			log.info("Received new customer keys : {}", message);
+			jwtKeys = Collections.unmodifiableMap(objectMapper.readValue(
+					message, new TypeReference<Map<Integer,String>>() {
+					}));
+			log.info("Successfully parsed token keys {}", jwtKeys);
+		} catch (JsonProcessingException e){
+			log.error("Failed to parse token keys from message: {}\nError: {}",message,e.getMessage());
+		} catch (Exception e){
+			log.error("Unexpected error while processing message from customer-keys topic: {}\nError: {}",
+					message, e.getMessage());
+		}
+	}
 
 
 	@KafkaListener(topics = "key-change", groupId = "${KAFKA_CONSUMER_GROUP_ID}")
