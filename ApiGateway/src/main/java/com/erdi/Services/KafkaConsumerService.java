e9c393b3414522047d4d47e5c2093c2e6bf67015
Erdi Syla <erdisyla6@gmail.com>
2025-05-26 15:20:56 +0200

    Commit 196: Changed KafkaConsumerServices' field jwtKeys type from a List to a Map for faster key retrieval based on the key id.

--- ApiGateway/src/main/java/com/erdi/Services/KafkaConsumerService.java
+++ ApiGateway/src/main/java/com/erdi/Services/KafkaConsumerService.java
@@ -1,43 +1,43 @@
 package com.erdi.Services;
 
-import com.erdi.DTOs.TokenKeyDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
-import java.util.List;
+import java.util.Map;
 
 @Getter
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class KafkaConsumerService {
 
-    private volatile List<TokenKeyDTO> jwtKeys = Collections.emptyList();
+    private volatile Map<Integer,String> jwtKeys = Collections.emptyMap();
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
     @KafkaListener(topics = {"validation-keys"}, groupId = "${KAFKA_CONSUMER_GROUP_ID}")
     public void listenAuthKeys(String message){
         try {
-            log.info("Received new customer keys : {}", message);
-            jwtKeys = Collections.unmodifiableList(objectMapper.readValue(
-                    message, new TypeReference<List<TokenKeyDTO>>() {
+            log.info("Received new validation keys : {}", message);
+            jwtKeys = Collections.unmodifiableMap(objectMapper.readValue(
+                    message, new TypeReference<Map<Integer,String>>() {
                     }));
             log.info("Successfully parsed validation keys {}", jwtKeys);
         } catch (JsonProcessingException e){
-            log.error("Failed to parse validation keys from message: {}\nError: {}",message,e.getMessage());
+            log.error("Failed to parse token keys from message: {}\nError: {}",message,e.getMessage());
         } catch (Exception e){
-            log.error("Unexpected error while processing message from validation-keys topic: {}\nError: {}",
+            log.error("Unexpected error while processing message from customer keys topic: {}\nError: {}",
                     message, e.getMessage());
         }
     }
+
 
     @KafkaListener(topics = "key-change", groupId = "${KAFKA_CONSUMER_GROUP_ID}")
     public void listenKeyChanged(String message){
